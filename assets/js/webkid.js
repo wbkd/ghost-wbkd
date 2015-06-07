b104d37b9903a6ae5b9016a4d32e285a41a310a7
Christopher <chrtze@gmail.com>
2015-06-07 17:33:41 +0200

    create taglist and sidebar

---
+++ assets/js/webkid.js
@@ -0,0 +1,65 @@
+$(function() {
+
+  var $articleContainer,
+      $tagContainer;
+
+
+  function renderSite(data) {
+    data = $.xml2json(data);
+    var posts = data.channel.item;
+
+    renderLatestArticles(posts);
+    renderTagList(posts);
+  }
+
+  function renderLatestArticles(posts) {
+
+    var $parent = $('.sidebox.latest-articles');
+    if(!$parent) {return};
+
+    for(var i = 0; i < 5; i++) {
+      var p = posts[i];
+      $parent.append('<p>' + p.title + '</p>');
+    }
+
+  }
+
+  function renderTagList(posts) {
+
+    var $parent = $('#taglist');
+    var tags = [];
+
+    if(!$parent) {return;}
+
+    posts.forEach(function(p,i) {
+
+      if($.isArray(p.category)) {
+        p.category.forEach(function(t,j) {
+
+          if(tags.indexOf(t) < 0) {
+            tags.push(t);
+          }
+
+        });
+      }
+
+    });
+
+    tags.forEach(function(t,i) {
+      var $li = $('<li/>');
+      var $a = $('<a/>');
+      $a.attr('href', '/tag/' + t);
+      $a.html(t);
+      $li.append($a);
+      $parent.append($li);
+    });
+
+  }
+
+  $.ajax({
+    dataType: 'xml',
+    url: '/rss',
+    type: 'GET'
+  }).success(renderSite);
+
+});
