2c5db036e50ade7a4c63513758b8955f959becd2
chrtze <chrtze@gmail.com>
2015-06-08 15:51:59 +0200

    sidebar styles

--- assets/js/webkid.js
+++ assets/js/webkid.js
@@ -1,54 +1,29 @@
 $(function() {
 
+  var months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'NOV', 'DEC'];
+
   function renderSite(data) {
     data = $.xml2json(data);
     var posts = data.channel.item;
-    
+   
     renderLatestArticles(posts);
-    //renderTagList(posts);
   }
 
   function renderLatestArticles(posts) {
-
+    console.log(posts);
     var $parent = $('.sidebox.latest-articles .sidebox-content');
     if(!$parent) {return};
 
     for(var i = 0; i < 5; i++) {
       var p = posts[i];
-      $parent.append('<p>' + p.title + '</p>');
-    }
-
-  }
-
-  function renderTagList(posts) {
-
-    var $parent = $('#taglist');
-    var tags = [];
-
-    if(!$parent) {return;}
-
-    posts.forEach(function(p,i) {
-
-      if($.isArray(p.category)) {
-        p.category.forEach(function(t,j) {
-
-          if(tags.indexOf(t) < 0) {
-            tags.push(t);
-          }
-
-        });
+      var date = new Date(p.pubDate);
+      var dateStr = date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
+      var $a = $('<a href="' + p.link + '"><div class="date">' + dateStr + '</div><div>' + p.title + '</div></a>');
+      if(i == 4) {
+        $a.addClass('last');
       }
-
-    });
-
-    tags.forEach(function(t,i) {
-      var $li = $('<li/>');
-      var $a = $('<a/>');
-      $a.attr('href', '/tag/' + t.replace(' ', '-'));
-      $a.html(t);
-      $li.append($a);
-      $parent.append($li);
-    });
+      $parent.append($a);
+    }
 
   }
 
